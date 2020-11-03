344f76692b61bcd42be91189f910bb881b200d31
Tiffany Kennedy <tr.kennedy@outlook.com>
2020-11-02 21:10:27 -0500

    Ha

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Header.css";
 
-const Header = ({ userList }) => {
+const Header = ({ currentUser, setCurrentUser, userList }) => {
+  const [selectedUser, setSelectedUser] = useState();
+
+  useEffect(() => {
+    setSelectedUser(userList[0]);
+  }, [userList]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
 
-  const handleSelectChange = (event) => {};
+  const handleSelectChange = (event) => {
+    const id = event.target.value;
+    const user = userList.find((user) => user.id == id);
+    setSelectedUser(user);
+  };
 
-  const handleUserLogin = (event) => {};
+  const handleUserLogin = (event) => {
+    setCurrentUser(selectedUser);
+  };
 
-  const handleUserLogout = (event) => {};
+  const handleUserLogout = (event) => {
+    setSelectedUser(userList[0]);
+    setCurrentUser(null);
+  };
 
   return (
     <header>
       <h1>Welcome to UserHub</h1>
       <form className="user-select" onSubmit={handleSubmit}>
-        <select onChange={handleSelectChange}>
-          {userList.map((user) => (
-            <option key={user.id} value={user.id}>
-              {user.username}
-            </option>
-          ))}
-        </select>
-        <button onClick={handleUserLogin}>LOG IN</button>
-        <button onClick={handleUserLogout}>LOG OUT</button>
+        {currentUser ? (
+          <button onClick={handleUserLogout}>
+            LOG OUT, {currentUser.username}
+          </button>
+        ) : (
+          <>
+            <select onChange={handleSelectChange}>
+              {userList.map((user) => (
+                <option key={user.id} value={user.id}>
+                  {user.username}
+                </option>
+              ))}
+            </select>
+            <button onClick={handleUserLogin}>LOG IN</button>
+          </>
+        )}
       </form>
     </header>
   );
